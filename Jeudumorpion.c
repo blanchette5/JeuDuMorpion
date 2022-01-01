4b2eeb483432aa6503990d961dc20729ca1b13bb
Auger Loïck <95275614+blanchette5@users.noreply.github.com>
2022-01-01 20:16:33 +0100

    Update Jeudumorpion.c

--- Jeudumorpion.c
+++ Jeudumorpion.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 // AUGER LOICK
-// CARPENTIER LUDIVINE 
+// CARPENTIER LUDIVINE
 // L2 INFO
 
  // Voici le code du jeu le morpion 3 par 3, pour l'executer telecharger le main
 char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 //Ensemble des caractères initialiser
+
+
+int main()
+{
+    int joueur = 1;
+    int i;
+    int choix;
+     // Voici le code du jeu le morpion 3 par 3, pour l'executer telecharger le main
 
 int gagne();
 //Fonction gagne permet de savoir si la partie a été remporter par un joueur ou a fini en match nul
@@ -14,9 +22,6 @@ int gagne();
 
 //Boucle plateau permettant le placement du choix saisie par l'utilisateur.
 void plateau();
-int main()
-{
-    int joueur = 1, i, choix;
 
     char implanter; //Caractère a implanter sur le plateau
     do
@@ -26,9 +31,7 @@ int main()
 
         printf("Joueur %d, entrez un chiffre:  ", joueur); // Affiche le numéro décidez au dessus pour savoir qui commence
         scanf("%d", &choix); // Scan le choix ( numéro entre 1 et 9)
-
-        implanter = (joueur == 1) ? 'X' : 'O'; // Symbole qui vont être utiliser dans l'affichage
-
+        implanter = (joueur == 1) ? 'x' : 'o'; // Symbole qui vont être utiliser dans l'affichage
         if (choix == 1 && square[1] == '1') // Si le choix est égale a 1 ainsi que la case 1, alors la case une prend la valeur implanter soit x ou o;
             square[1] = implanter;
         else if (choix == 2 && square[2] == '2')
@@ -47,15 +50,11 @@ int main()
             square[8] = implanter;
         else if (choix == 9 && square[9] == '9')
             square[9] = implanter;
-
         else //Sinon
         {
-            printf("Saisie incorrect ");
-
             joueur--; //Demande de rejoué au joueur 1
-            getch(); //Affichage caractères
-        }
 
+        }
         i = gagne(); // I nous sert d'indice : il prend la valeur de la fonction gagne
         joueur++;// Joueur suivant
     }while (i ==  - 1);// Tant que i = -1
@@ -64,8 +63,6 @@ int main()
         printf("==>\aJoueur %d win ", --joueur);// le joueur qui a jouer le dernier tour est affiché vainqueur.
     else
         printf("==>\aPartie nul"); // Sinon partie nul
-    getch(); // Renvoi de notre affichage
-
     return 0;
 }
 
@@ -79,28 +76,20 @@ int gagne()
 {
     if (square[1] == square[2] && square[2] == square[3]) //Premier alignement  1 2 3
         return 1; // Retourne 1 si les mêmes symbole sont sur les cases 1,2 et 3
-
     else if (square[4] == square[5] && square[5] == square[6])
         return 1;
-
     else if (square[7] == square[8] && square[8] == square[9])
         return 1;
-
     else if (square[1] == square[4] && square[4] == square[7])
         return 1;
-
     else if (square[2] == square[5] && square[5] == square[8])
         return 1;
-
     else if (square[3] == square[6] && square[6] == square[9])
         return 1;
-
     else if (square[1] == square[5] && square[5] == square[9])
         return 1;
-
     else if (square[3] == square[5] && square[5] == square[7])
         return 1;
-
     else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
         square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
         != '7' && square[8] != '8' && square[9] != '9')
